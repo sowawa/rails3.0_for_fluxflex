d105e95f74febaab739465f272e361f0cabcbbab
sowawa ( Keisuke SOGAWA ) <keisuke.sogawa@gmail.com>
2011-06-27 22:25:39 +0900

    rails 3.0 for fluxflex.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSample::Application.config.secret_token = 'a357bd6fb357e22ab311bc252da7c9f3a6c23975a9a39107e1b4c51cb3886134e3f972707172a027a0ccfbbf5d3cda90e8807ead1e373badf0b451cbdbc76dc0'
