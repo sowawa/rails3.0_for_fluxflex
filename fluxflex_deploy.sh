6602c423a68371a25f0e1ee97b3df3c2221db938
Tomohiro Nishimura <tomohiro68@gmail.com>
2011-06-29 23:31:16 +0900

    tweaked a setup/deploy script

--- fluxflex_deploy.sh
+++ fluxflex_deploy.sh
@@ -2,10 +2,6 @@ export GEM_HOME=$HOME/.gem/ruby/1.9.1/
 export GEM_PATH=$GEM_HOME
 export PATH=$PATH:/usr/local/rvm/rubies/ruby-1.9.2-p180/bin:$HOME/.gem/ruby/1.9.1/bin
 
-gem install rails --no-ri --no-rdoc
-gem install mysql --no-ri --no-rdoc
-gem install bundler --no-ri --no-rdoc
-
 bundle install
 
-rake db:migrate:reset RAILS_ENV=production
+rake db:migrate RAILS_ENV=production
